4e3cc8e8b6f8dfc07789050f8f58db9e0795c9a3
Deyan Atanasov <deyan.atanasov@movedigital.ch>
2023-06-17 16:14:59 +0300

    feat: add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { Routes } from './Routes';
+
 const MOUNT_NODE = document.getElementById('root');
 
 if (!MOUNT_NODE) {
@@ -11,9 +13,8 @@ const root = createRoot(MOUNT_NODE)
 
 const render = () => {
   root.render(
-    <>S3 bucket browser</>
+    <Routes/>
   )
 }
-
 
 render()
---
+++ src/Routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, BrowserRouter, Routes as RouterRoutes } from 'react-router-dom'
+
+import { LoginPage } from './components/pages/LoginPage'
+import { HomePage } from './components/pages/HomePage'
+
+export const Routes = () => (
+  <BrowserRouter>
+    <RouterRoutes>
+      <Route
+        path='/login'
+        element={<LoginPage/>}
+      />
+
+      <Route
+        path='/'
+        element={<HomePage/>}
+      />
+    </RouterRoutes>
+  </BrowserRouter>
+)
