ff6f4169e4d67a38c46752c15f1778e9ceea6dae
Deyan Atanasov <deyan.atanasov@movedigital.ch>
2023-06-19 22:06:03 +0300

    feat: add useAuthState hook

--- src/store/storeFacade.ts
+++ src/store/storeFacade.ts
@@ -11,3 +11,8 @@ export const useLoadingState = (actionName: string): boolean =>  useAppSelector(
   (state) => state.networkState.loading[actionName],
   shallowEqual,
 );
+
+export const useAuthState = () => useAppSelector(
+  (state) => state.authState,
+  shallowEqual,
+);
