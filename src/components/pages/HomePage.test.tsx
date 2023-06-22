dfebfa0f9bf23617045790a49ff14ab18c744915
Deyan Atanasov <deyan.atanasov@movedigital.ch>
2023-06-22 14:00:10 +0300

    lint

--- src/components/pages/HomePage.test.tsx
+++ src/components/pages/HomePage.test.tsx
@@ -1,12 +1,12 @@
-import { describe, test, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { describe, test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
 
-import { HomePage } from './HomePage'
+import { HomePage } from './HomePage';
 
 describe('<HomePage/>', () => {
   test('renders component', () => {
-    render(<HomePage/>)
+    render(<HomePage/>);
 
-    expect(screen.getByText(/home page/i)).toBeInTheDocument()
-  })
-})
+    expect(screen.getByText(/home page/i)).toBeInTheDocument();
+  });
+});
