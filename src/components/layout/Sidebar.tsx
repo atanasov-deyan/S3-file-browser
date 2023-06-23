1c6e5e24ffaf691d9bae50aa5adcb442ec88c098
Deyan Atanasov <deyan.atanasov@movedigital.ch>
2023-06-23 08:55:38 +0300

    feat: sidebar

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react';
+
+import { toClassName } from '../../utils/toClassName';
+
+import styles from './Sidebar.module.css' ;
+
+interface ISidebarProps {
+  className?: string,
+  children: ReactNode,
+}
+
+export const Sidebar = ({ className, children }: ISidebarProps) => {
+
+  const sidebarClassName = toClassName([styles.sidebar, className]);
+
+  return (
+    <aside className={sidebarClassName}>
+      {children}
+    </aside>
+  );
+};
