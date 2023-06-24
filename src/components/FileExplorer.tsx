108423205b9f3f8a8835fe5227cb93069f15a3fc
Deyan Atanasov <deyan.atanasov@movedigital.ch>
2023-06-24 20:30:20 +0300

    feat: file explorer

--- src/components/FileExplorer.tsx
+++ src/components/FileExplorer.tsx
@@ -1,42 +1,16 @@
-import { MouseEvent } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import { useFilesState, useLoadingState } from '../store/storeFacade';
-import { isFile } from '../utils/fileSystem';
-import { Icon } from './layout/Icon';
-import { toClassName } from '../utils/toClassName';
-import { Button } from './layout/Button';
 import { Spin } from './layout/Spin';
+import { FileExplorerItem } from './FileExplorerItem';
 
 import styles from './FileExplorer.module.css';
-import { ROOT_DIR_NAME } from '../config';
-
-const getIconName = (name: string): string => {
-  return isFile(name) ? 'file-o' : 'folder';
-};
-
-const getClassName = (name: string): string => {
-  return isFile(name)
-    ? styles.icon
-    : toClassName([styles.icon, styles['folder-icon']]);
-};
 
 export const FileExplorer = () => {
   const { pathname } = useLocation();
-  const navigate = useNavigate()
   const { filesTree } = useFilesState();
   const isLoading = useLoadingState('files/files');
   const content = filesTree[pathname];
-  const handleClick = (name: string) => (e: MouseEvent) => {
-    const isDir = !isFile(name);
-
-    if (e.detail === 2) {
-      if (isDir) {
-        const folderPath = pathname === ROOT_DIR_NAME ? '' : pathname;
-        navigate(`${folderPath}/${name}`);
-      }
-    }
-  };
 
   if (isLoading) {
     return (
@@ -52,18 +26,7 @@ export const FileExplorer = () => {
     <ul className={styles['folder-items']}>
       {content.map((name: string) => (
         <li key={name} className={styles['list-item']}>
-          <Button
-            type='text'
-            onClick={handleClick(name)}
-            className={styles.button}
-            >
-              <div className={styles.item}>
-                <Icon name={getIconName(name)} className={getClassName(name)}/>
-                <div className={styles['item-name']} title={name}>
-                  {name}
-                </div>
-              </div>
-            </Button>
+          <FileExplorerItem name={name}/>
         </li>
         ))}
     </ul>
