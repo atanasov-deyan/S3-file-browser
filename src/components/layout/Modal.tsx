5be3cef80c19d0568fd0af250995fd75186dfc26
Deyan Atanasov <deyan.atanasov@movedigital.ch>
2023-06-26 12:04:49 +0300

    feat: add modal component

---
+++ src/components/layout/Modal.tsx
@@ -0,0 +1,27 @@
+import { MutableRefObject, ReactNode, useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+export const Modal = ({ children }: { children: ReactNode }) => {
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement('div');
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById('modal');
+
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
+
+    modalRoot.appendChild(elRef.current);
+
+    return () => {
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
+    };
+  }, []);
+
+  return createPortal(children, elRef.current);
+};
