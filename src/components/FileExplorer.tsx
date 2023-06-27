5cb91b582364875215a67b3ee6adbb8d25dbd617
Deyan Atanasov <deyan.atanasov@movedigital.ch>
2023-06-27 22:44:52 +0300

    feat: if user deletes a dir tree - redirect them to the closest node

--- src/components/FileExplorer.tsx
+++ src/components/FileExplorer.tsx
@@ -1,16 +1,29 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import { useFilesState, useLoadingState } from '../store/storeFacade';
 import { Spin } from './layout/Spin';
 import { FileExplorerItem } from './FileExplorerItem';
+import { getNextAvailablePath } from '../utils/fileSystem';
 
 import styles from './FileExplorer.module.css';
 
 export const FileExplorer = () => {
   const { pathname } = useLocation();
   const { filesTree } = useFilesState();
+  const navigate = useNavigate();
   const isLoading = useLoadingState('files/files');
   const content = filesTree[pathname];
+
+  useEffect(() => {
+    // upon initial boot we do not want to redirect the user since we will always only have the empty root dir;
+    const tryToRedirect = !isLoading && typeof isLoading !== undefined;
+    if (tryToRedirect && !filesTree[pathname]) {
+      const redirectTo = getNextAvailablePath(pathname, filesTree);
+      console.log('are you there')
+      navigate(redirectTo);
+    }
+  }, [isLoading, pathname, filesTree])
 
   if (isLoading) {
     return (
